8edbe6fe3d7cccd6684b23223ffc8543812f5d37
177sma <howerds4my@gmail.com>
2019-05-20 16:46:21 -0400

    Draw Clip Plane on mirror instead

---
+++ Mirror/Assets/Scripts/ClipPlaneDrawer.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClipPlaneDrawer : MonoBehaviour
+{
+    public GameObject[] vertices;
+    public Plane[] planes = new Plane[5];
+
+    private Camera mainCam;
+
+    private void Awake()
+    {
+        mainCam = Camera.main;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        DrawPlane();
+    }
+
+    public void DrawPlane()
+    {
+        planes[0] = new Plane(mainCam.transform.position, vertices[0].transform.position, vertices[1].transform.position);
+        planes[1] = new Plane(mainCam.transform.position, vertices[2].transform.position, vertices[3].transform.position);
+        planes[2] = new Plane(mainCam.transform.position, vertices[3].transform.position, vertices[1].transform.position);
+        planes[3] = new Plane(mainCam.transform.position, vertices[1].transform.position, vertices[2].transform.position);
+        planes[4] = new Plane(vertices[0].transform.position, vertices[3].transform.position, vertices[2].transform.position);
+    }
+}
--- Mirror/Assets/Scripts/ValveMovement.cs
+++ Mirror/Assets/Scripts/ValveMovement.cs
@@ -10,12 +10,12 @@ using Vector3 = UnityEngine.Vector3;
 public class ValveMovement : MonoBehaviour
 {
     public int planeFollow;
-    public GameObject[] vertices;
+    public ClipPlaneDrawer mirrorClipPlane;
     private Camera mainCam;
     private Vector3 oriScale;
     private NavMeshModifier navMod;
 
-    public Plane[] planes = new Plane[5];
+
 
     private void Awake()
     {
@@ -38,27 +38,23 @@ public class ValveMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        planes[0] = new Plane(mainCam.transform.position, vertices[0].transform.position, vertices[1].transform.position);
-        planes[1] = new Plane(mainCam.transform.position, vertices[2].transform.position, vertices[3].transform.position);
-        planes[2] = new Plane(mainCam.transform.position, vertices[3].transform.position, vertices[1].transform.position);
-        planes[3] = new Plane(mainCam.transform.position, vertices[1].transform.position, vertices[2].transform.position);
-        planes[4] = new Plane(vertices[0].transform.position, vertices[3].transform.position, vertices[2].transform.position);
+
         
         Ray ray1 = new Ray(transform.position, Vector3.back);
         Debug.DrawRay(ray1.origin, ray1.direction, Color.green);
-        if (planes[planeFollow].Raycast(ray1, out float dist1))
+        if (mirrorClipPlane.planes[planeFollow].Raycast(ray1, out float dist1))
         {
             transform.position = ray1.GetPoint(dist1);  
         }
         
         Ray ray2 = new Ray(transform.position, Vector3.forward);
         Debug.DrawRay(ray2.origin, ray2.direction, Color.green);
-        if (planes[planeFollow].Raycast(ray2, out float dist2))
+        if (mirrorClipPlane.planes[planeFollow].Raycast(ray2, out float dist2))
         {
             transform.position = ray2.GetPoint(dist2);  
         }
 
-        transform.rotation = Quaternion.LookRotation(planes[planeFollow].normal);
+        transform.rotation = Quaternion.LookRotation(mirrorClipPlane.planes[planeFollow].normal);
 
         transform.localScale = new Vector3(oriScale.x / Mathf.Cos(transform.localEulerAngles.y * Mathf.Deg2Rad) * 0.9f, oriScale.y, oriScale.z);
         
