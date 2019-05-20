d783215991eb66bc74b3b6a9704fed4fdcf9aef1
177sma <howerds4my@gmail.com>
2019-05-20 16:23:07 -0400

    Valve enhancement

--- Mirror/Assets/Scripts/ValveMovement.cs
+++ Mirror/Assets/Scripts/ValveMovement.cs
@@ -9,11 +9,13 @@ using Vector3 = UnityEngine.Vector3;
 
 public class ValveMovement : MonoBehaviour
 {
-    
+    public int planeFollow;
     public GameObject[] vertices;
     private Camera mainCam;
     private Vector3 oriScale;
     private NavMeshModifier navMod;
+
+    public Plane[] planes = new Plane[5];
 
     private void Awake()
     {
@@ -36,23 +38,27 @@ public class ValveMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Plane leftPlane = new Plane(mainCam.transform.position, vertices[0].transform.position, vertices[1].transform.position);
+        planes[0] = new Plane(mainCam.transform.position, vertices[0].transform.position, vertices[1].transform.position);
+        planes[1] = new Plane(mainCam.transform.position, vertices[2].transform.position, vertices[3].transform.position);
+        planes[2] = new Plane(mainCam.transform.position, vertices[3].transform.position, vertices[1].transform.position);
+        planes[3] = new Plane(mainCam.transform.position, vertices[1].transform.position, vertices[2].transform.position);
+        planes[4] = new Plane(vertices[0].transform.position, vertices[3].transform.position, vertices[2].transform.position);
         
         Ray ray1 = new Ray(transform.position, Vector3.back);
         Debug.DrawRay(ray1.origin, ray1.direction, Color.green);
-        if (leftPlane.Raycast(ray1, out float dist1))
+        if (planes[planeFollow].Raycast(ray1, out float dist1))
         {
             transform.position = ray1.GetPoint(dist1);  
         }
         
         Ray ray2 = new Ray(transform.position, Vector3.forward);
         Debug.DrawRay(ray2.origin, ray2.direction, Color.green);
-        if (leftPlane.Raycast(ray2, out float dist2))
+        if (planes[planeFollow].Raycast(ray2, out float dist2))
         {
             transform.position = ray2.GetPoint(dist2);  
         }
 
-        transform.rotation = Quaternion.LookRotation(leftPlane.normal);
+        transform.rotation = Quaternion.LookRotation(planes[planeFollow].normal);
 
         transform.localScale = new Vector3(oriScale.x / Mathf.Cos(transform.localEulerAngles.y * Mathf.Deg2Rad) * 0.9f, oriScale.y, oriScale.z);
         
--- Mirror/Assets/Scripts/ValveSafeZone.cs
+++ Mirror/Assets/Scripts/ValveSafeZone.cs
@@ -18,7 +18,7 @@ public class ValveSafeZone : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.localScale = new Vector3(transform.localScale.x, transform.localScale.y, Mathf.Clamp(transform.position.z - valve.transform.position.z, lMin, lMax));
+        transform.localScale = new Vector3(transform.localScale.x, transform.localScale.y, Mathf.Clamp(Mathf.Abs(transform.position.z - valve.transform.position.z), lMin, lMax));
     }
 
     /*private void OnTriggerEnter(Collider other)
