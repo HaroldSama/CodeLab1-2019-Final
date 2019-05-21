71d793afd4a3b4a772711e8194d4bcbbd1f0fbab
177sma <howerds4my@gmail.com>
2019-05-21 04:07:53 -0400

    Fence

---
+++ Mirror/Assets/Scripts/FenceMovement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FenceMovement : MonoBehaviour
+{
+    public GameObject mirror;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float scale = Mathf.Abs(mirror.transform.position.z - transform.position.z) - 2;
+        transform.localScale = new Vector3(scale, transform.localScale.y, transform.localScale.z);
+    }
+}
